bc168d280738667af9e6618171e20a1f75c26a0a
mahmosa1 <162572031+mahmosa1@users.noreply.github.com>
2024-03-30 16:52:21 +0300

    main

--- Civic_swiftP/CivicSwiftProject/settings.py
+++ Civic_swiftP/CivicSwiftProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CivicSwiftProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': ['Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
