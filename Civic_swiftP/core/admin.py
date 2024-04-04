7c8309ba78b72737028909690e29c0a82519e0ed
mahmosa1 <162572031+mahmosa1@users.noreply.github.com>
2024-04-04 04:06:36 +0300

    Update admin.py

--- Civic_swiftP/core/admin.py
+++ Civic_swiftP/core/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Room, Message
+
 
 # Register your models here.
-admin.site.register(Room)
-admin.site.register(Message)
-
-def delete_selected_users(modeladmin, request, queryset):
-    for user in queryset:
-        user.delete()
-
-delete_selected_users.short_description = "Delete selected users"  # Sets the display name for the action
-
-# Register the custom action for the User model
-admin.site.add_action(delete_selected_users)
-
-# Register the User model with the admin interface
-admin.site.unregister(User)  # Unregister the User model first if it's already registered by default
-admin.site.register(User)
