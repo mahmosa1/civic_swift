cef5990a9e1686309afc54c8673378842c541f51
mahmosa1 <162572031+mahmosa1@users.noreply.github.com>
2024-04-04 04:08:21 +0300

    Update models.py

--- Civic_swiftP/core/models.py
+++ Civic_swiftP/core/models.py
@@ -16,12 +16,3 @@ class Resident(models.Model):
 
     def __str__(self):
         return self.user.username
-
-class Room(models.Model):
-    name = models.CharField(max_length=1000)
-
-class Message(models.Model):
-    value = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
-    user = models.CharField(max_length=1000000)
-    room = models.CharField(max_length=1000000)
